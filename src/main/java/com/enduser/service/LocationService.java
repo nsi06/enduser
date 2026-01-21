4d1e001e67a074cc3b1f568612dcde3f99147dd9
Navjeet Singh <navjeet.singh@sky.uk>
2026-01-21 15:47:29 +0000

    add logger

--- src/main/java/com/enduser/service/LocationService.java
+++ src/main/java/com/enduser/service/LocationService.java
@@ -1,10 +1,15 @@
 package com.enduser.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocationService {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
     public void processLocationUpdate(String location) {
-        System.out.println("Received location update: " + location);    }
+        this.logger.info("Received location update: " + location);    }
 }
